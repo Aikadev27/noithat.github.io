9777d1dc61a2964099dde6abeb5274a6229a872c
Aikadev <aikadev27@gmail.com>
2022-04-15 14:25:14 +0700

    15/04 header and slider

---
+++ main.js
@@ -0,0 +1,32 @@
+/**
+ * ! fixed header_bottom
+ */
+window.onscroll = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 100) {
+        document.querySelector(".header_bottom").classList.add("active");
+    } else {
+        document.querySelector(".header_bottom").classList.remove("active");
+    }
+};
+/*
+!slider 
+*/
+//  slider
+$(document).ready(function() {
+    $(".slider").owlCarousel({
+        loop: true,
+
+        nav: true,
+        items: 1,
+        animateOut: "slideOutDown",
+        animateIn: "flipInX",
+        items: 1,
+
+
+        smartSpeed: 450,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+    });
+});
